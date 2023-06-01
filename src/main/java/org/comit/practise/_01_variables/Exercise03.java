1f269c1a277818805ab78921cee613dd45a18cd8
Saraswathi Sakthikumar <saraswathi1404@gmail.com>
2023-06-01 12:51:27 -0600

    Exercise 07

---
+++ src/main/java/org/comit/practise/_01_variables/Exercise03.java
@@ -0,0 +1,16 @@
+package org.comit.practise._01_variables;
+
+public class Exercise03 {
+
+	public static void main(String[] args) {
+		//Write a program in Java that prints a float number, for example, the 5.3 or the 7.5.
+		
+		double num1 = 5.3;
+		double num2 = 7.5;
+		
+		System.out.println("The first number is :" + num1);
+		System.out.println("The second number is :" + num2);
+
+	}
+
+}
