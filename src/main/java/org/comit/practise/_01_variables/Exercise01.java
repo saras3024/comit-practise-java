4ef03fe41b270cd4f7fd4c046bd4fb1292894d7d
Saraswathi Sakthikumar <saraswathi1404@gmail.com>
2023-06-01 10:45:34 -0600

    First Practise Exercise

---
+++ src/main/java/org/comit/practise/_01_variables/Exercise01.java
@@ -0,0 +1,12 @@
+package org.comit.practise._01_variables;
+
+public class Exercise01 {
+
+	public static void main(String[] args) {
+		//Write a program in Java that prints the phrase "Hi, I already print sentences."
+		//This is Exercise01
+		System.out.println("Hi, I already print sentences.");
+
+	}
+
+}
